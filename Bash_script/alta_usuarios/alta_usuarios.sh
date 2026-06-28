065d26f854728c8f1629f15d63f51fd189ea2eab
Agustin Sanchez <agustin.sanchez1994@gmail.com>
2026-06-28 18:18:59 +0000

    R3: Agrego script de alta de usuarios

--- Bash_script/alta_usuarios/alta_usuarios.sh
+++ Bash_script/alta_usuarios/alta_usuarios.sh
@@ -14,17 +14,17 @@ clear
 ###############################
 
 LISTA=$1
-USUARIO=$2
+USER=$2
 
 ANT_IFS=$IFS
 IFS=$'\n'
 for LINEA in `cat $LISTA |  grep -v ^#`
 do
-	USUARIO=$(echo  $LINEA |awk -F ':' '{print $1}')
-	GRUPO=$(echo  $LINEA |awk -F ':' '{print $2}')
-	echo "sudo useradd -m -s /bin/bash -g $GRUPO $USUARIO -p {sudo cat /etc/$USUARIO | grep "$USUARIO" | awk -F ':' '{print $2}'
-}"
-
+	USUARIO=$(echo  $LINEA |awk -F ',' '{print $1}')
+	GRUPO=$(echo  $LINEA |awk -F ',' '{print $2}')
+	HOME_DIR=$(echo  $LINEA |awk -F ',' '{print $3}')
+	sudo groupadd -f $GRUPO
+	sudo useradd -m -b /work -s /bin/bash -g $GRUPO $USUARIO -p $(sudo cat /etc/shadow | grep "^$USER:" | awk -F ':' '{print $2}') #############=="^$USER:" -> "exactamente igual a" ^xx:
 done
 IFS=$ANT_IFS
 
