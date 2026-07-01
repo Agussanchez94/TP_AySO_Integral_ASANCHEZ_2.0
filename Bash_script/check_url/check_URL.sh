1f6cd6c933b852db42566e7f3f5a6abb16c4cbe3
Agustin Sanchez <agustin.sanchez1994@gmail.com>
2026-07-01 02:02:25 +0000

    R3: modificando Check_URL

--- Bash_script/check_url/check_URL.sh
+++ Bash_script/check_url/check_URL.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+i#!/bin/bash
 clear
 
 ###############################
@@ -9,7 +9,7 @@ clear
 #  Tareas:
 #  - Se debera generar la estructura de directorio pedida con 1 solo comando con las tecnicas enseñadas en clases
 #  - Generar los archivos de logs requeridos.
-#
+#i
 ###############################
 LISTA=$1
 
@@ -18,19 +18,33 @@ LOG_FILE="/var/log/status_url.log"
 ANT_IFS=$IFS
 IFS=$'\n'
 
+for LINEA in `cat $LISTA|grep -v ^#`
+do
 #---- Dentro del bucle ----#
   # Obtener el código de estado HTTP
+  URL=$(echo $LINEA | awk '{print $2}')
   STATUS_CODE=$(curl -LI -o /dev/null -w '%{http_code}\n' -s "$URL")
-
+  DOMINIO=$(echo $LINEA | awk '{print $1}')
   # Fecha y hora actual en formato yyyymmdd_hhmmss
   TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-
 
  # Registrar en el archivo /var/log/status_url.log
   echo "$TIMESTAMP - Code:$STATUS_CODE - URL:$URL" |sudo tee -a  "$LOG_FILE"
 
+case $STATUS_CODE in
+        200)
+                echo "$TIMESTAMP - Code:$STATUS_CODE - URL:$URL" | sudo tee -a /tmp/head-check/ok/$DOMINIO.log
+                ;;
+        4[0-9][0-9]|000)
+            echo "$TIMESTAMP - Code:$STATUS_CODE - URL:$URL" | sudo tee -a /tmp/head-check/Error/cliente/$DOMINIO.log
+	    echo"noexiste"
+            ;;
+    5[0-9][0-9])
+            echo "$TIMESTAMP - Code:$STATUS_CODE - URL:$URL" | sudo tee -a /tmp/head-check/Error/servidor/$DOMINIO.log
+            ;;
+    esac
 
 
 #-------------------------#
-
+done
 IFS=$ANT_IFS
